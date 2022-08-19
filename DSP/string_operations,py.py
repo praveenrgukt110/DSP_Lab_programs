57308962502596c58278dfd72c83b4a8e4deb610
praveenrgukt110 <79621150+praveenrgukt110@users.noreply.github.com>
2022-08-19 23:13:02 +0530

    Add files via upload

---
+++ DSP/string_operations,py.py
@@ -0,0 +1,23 @@
+str="PRAVEEN"
+print(str.isupper())
+print(str.islower())
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.isalpha())
+print(str.partition('e'))
+print(str.split('A'))
+print(str.isdigit())
+print(str.count('a'))
+print(len(str))
+print(str[:5])
+print(str[-2])
+print(str[-1:-3])
+print(str.startswith("P"))
+string="HeLlo"
+string.swapcase()
+string="I Like bananas"
+string.replace("bananas","apples")
+string.index('i')
+print("ke" in string)
+print(string.isalpha())
